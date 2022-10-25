343408ee1e74ea5f1080095bd93d64634c20fd0a
cb271805 <MY_NAME@example.com>
2022-10-25 10:30:29 +0200

    moved the subject# to nip dict in a separate file for easier maintaining

--- bids_formatting/formatting.py
+++ bids_formatting/formatting.py
@@ -31,6 +31,7 @@ import re
 import mne
 from pathlib import Path
 from mne_bids import BIDSPath, write_raw_bids
+from CONST import dict_nip_to_sn
 
 # Copying the new files first to the raw directory:
 
@@ -50,16 +51,16 @@ TASK = 'listen'
 # We need to transform our NIP into an anynonimized number, refering to:
 # https://cbel.atlassian.net/wiki/spaces/NEUROSPIN/pages/edit-v2/12648452
 
-dict_nip_to_sn = {'ae_140329': '2', 'cc_150418': '3', 'cl_220500': '5',
-                  'fr_190151': '7', 'hg_220389': '8', 'js_180232': '9',
-                  'kc_160388': '10', 'kp_210315': '11', 'lg_170436': '12',
-                  'lq_180242': '13', 'mb_180076': '14', 'mf_180020': '15',
-                  'ml_100438': '16', 'ml_180010': '17', 'ml_220421': '18',
-                  'pl_170230': '19', 'rt_220104': '20', 'sa_170217': '21',
-                  'sf_180213': '22', 'eg_220435': '23', 'cl_190429': '24',
-                  'ap_220150': '25', 'df_130078': '26', 'lp_090137': '4',
-                  'ya_170284': '6',
-                  }
+# dict_nip_to_sn = {'ae_140329': '2', 'cc_150418': '3', 'cl_220500': '5',
+#                   'fr_190151': '7', 'hg_220389': '8', 'js_180232': '9',
+#                   'kc_160388': '10', 'kp_210315': '11', 'lg_170436': '12',
+#                   'lq_180242': '13', 'mb_180076': '14', 'mf_180020': '15',
+#                   'ml_100438': '16', 'ml_180010': '17', 'ml_220421': '18',
+#                   'pl_170230': '19', 'rt_220104': '20', 'sa_170217': '21',
+#                   'sf_180213': '22', 'eg_220435': '23', 'cl_190429': '24',
+#                   'ap_220150': '25', 'df_130078': '26', 'lp_090137': '4',
+#                   'ya_170284': '6', 'vc_200442': '27', 'cj_090334': '1'
+#                   }
 
 
 """
