811b091e926a4e91899c08810fd3b4138b7cb6b3
co <corentin.bel@protonmail.com>
2022-12-01 15:31:25 +0100

    added text input to be iterated on gpt2

--- meg-masc/check_decoding.py
+++ meg-masc/check_decoding.py
@@ -36,6 +36,7 @@ from TO_EXCLUDE import to_exclude
 import matplotlib.pyplot as plt
 from pathlib import Path
 import matplotlib
+
 matplotlib.use("Agg")
 mne.set_log_level(False)
 
@@ -68,15 +69,15 @@ class PATHS:
             f.write(str(data) + "\n")
     with open(path_file, "r") as f:
         data = Path(f.readlines()[0].strip("\n"))
-        print(f'File opened: {data}')
-        if str(data).__contains__('final'):
+        print(f"File opened: {data}")
+        if str(data).__contains__("final"):
             print("Processed data (Maxwell filtered) used")
-        if str(data).__contains__('BIDS'):
+        if str(data).__contains__("BIDS"):
             print("Raw data (no filtering) used")
     # assert data.exists()
 
 
-TASK = 'listen'
+TASK = "listen"
 # To simplify for the time being
 # To run on the Neurospin workstation
 PATHS.data = Path("/home/is153802/workspace_LPP/data/MEG/LPP/BIDS_final")
@@ -100,25 +101,25 @@ def epoch_data(subject, run_id):
 
     # define path
     # Two cases: running on raw or filtered data
-    if str(PATHS.data).__contains__('derivatives'):
+    if str(PATHS.data).__contains__("derivatives"):
         print("Running the script on FILTERED data:")
-        print(f'run {run_id}, subject: {subject}')
+        print(f"run {run_id}, subject: {subject}")
         bids_path = mne_bids.BIDSPath(
             subject=subject,
-            session='01',
+            session="01",
             task=TASK,
             datatype="meg",
             root=PATHS.data,
             run=run_id,
-            processing='sss',
+            processing="sss",
         )
     # elif str(PATHS.data).__contains__('BIDS'):
     else:
         print("Running the script on RAW data:")
-        print(f'run {run_id}, subject: {subject}')
+        print(f"run {run_id}, subject: {subject}")
         bids_path = mne_bids.BIDSPath(
             subject=subject,
-            session='01',
+            session="01",
             task=TASK,
             datatype="meg",
             root=PATHS.data,
@@ -128,37 +129,39 @@ def epoch_data(subject, run_id):
     raw = mne_bids.read_raw_bids(bids_path)
     raw.pick_types(meg=True, stim=True)
     raw.load_data()
-    raw = raw.filter(.5, 20)
+    raw = raw.filter(0.5, 20)
 
-    event_file = PATHS.data/f'sub-{bids_path.subject}'
-    event_file = event_file / f'ses-{bids_path.session}'
-    event_file = event_file / 'meg'
-    event_file = str(event_file / f'sub-{bids_path.subject}')
-    event_file += f'_ses-{bids_path.session}'
-    event_file += f'_task-{bids_path.task}'
-    event_file += f'_run-{bids_path.run}_events.tsv'
-    assert (Path(event_file).exists())
+    event_file = PATHS.data / f"sub-{bids_path.subject}"
+    event_file = event_file / f"ses-{bids_path.session}"
+    event_file = event_file / "meg"
+    event_file = str(event_file / f"sub-{bids_path.subject}")
+    event_file += f"_ses-{bids_path.session}"
+    event_file += f"_task-{bids_path.task}"
+    event_file += f"_run-{bids_path.run}_events.tsv"
+    assert Path(event_file).exists()
     # read events
-    meta = pd.read_csv(event_file, sep='\t')
-    events = mne.find_events(raw, stim_channel='STI101',
-                             shortest_event=1, min_duration=0.0010001)
+    meta = pd.read_csv(event_file, sep="\t")
+    events = mne.find_events(
+        raw, stim_channel="STI101", shortest_event=1, min_duration=0.0010001
+    )
 
     # match events and metadata
     word_events = events[events[:, 2] > 1]
-    meg_delta = np.round(np.diff(word_events[:, 0]/raw.info['sfreq']))
+    meg_delta = np.round(np.diff(word_events[:, 0] / raw.info["sfreq"]))
     meta_delta = np.round(np.diff(meta.onset.values))
 
     print(events)
     print(meta.onset.values)
     i, j = match_list(meg_delta, meta_delta)
-    print(f'Len i : {len(i)} for run {run_id}')
+    print(f"Len i : {len(i)} for run {run_id}")
     assert len(i) > 500
     events = word_events[i]
     # events = events[i]  # events = words_events[i]
     meta = meta.iloc[j].reset_index()
 
-    epochs = mne.Epochs(raw, events, metadata=meta,
-                        tmin=-.3, tmax=.8, decim=10, baseline=(-0.2, 0.0))
+    epochs = mne.Epochs(
+        raw, events, metadata=meta, tmin=-0.3, tmax=0.8, decim=10, baseline=(-0.2, 0.0)
+    )
 
     data = epochs.get_data()
     epochs.load_data()
@@ -173,8 +176,11 @@ def epoch_data(subject, run_id):
     # To try: sigmas = 7 or 15
     sigma = 7
     vec = np.clip(vec, -sigma, sigma)
-    epochs._data[:, :, :] = scaler.inverse_transform(vec)\
-        .reshape(n_words, n_times, n_chans).transpose(0, 2, 1)
+    epochs._data[:, :, :] = (
+        scaler.inverse_transform(vec)
+        .reshape(n_words, n_times, n_chans)
+        .transpose(0, 2, 1)
+    )
 
     return epochs
 
@@ -182,15 +188,14 @@ def epoch_data(subject, run_id):
 def decod(X, y):
     assert len(X) == len(y)
     # define data
-    model = make_pipeline(StandardScaler(),
-                          RidgeCV(alphas=np.logspace(-3, 8, 10)))
+    model = make_pipeline(StandardScaler(), RidgeCV(alphas=np.logspace(-3, 8, 10)))
     cv = KFold(5, shuffle=True, random_state=0)
 
     # fit predict
     n, n_chans, n_times = X.shape
     R = np.zeros(n_times)
     for t in range(n_times):
-        print('.', end='')
+        print(".", end="")
         y_pred = cross_val_predict(model, X[:, :, t], y, cv=cv)
         R[t] = correlate(y, y_pred)
     return R
@@ -289,25 +294,23 @@ if __name__ == "__main__":
     print("\nSubjects for which the decoding will be tested: \n")
     print(subjects)
 
-    
-
     for subject in subjects:
 
         if subject in to_exclude:
             continue
 
-        print(f'Subject {subject}\'s decoding started')
+        print(f"Subject {subject}'s decoding started")
         epochs = []
-        for run_id in range(1, RUN+1):
-            print('.', end='')
-            epo = epoch_data(subject, '%.2i' % run_id)
-            epo.metadata['label'] = f"run_{run_id}"
+        for run_id in range(1, RUN + 1):
+            print(".", end="")
+            epo = epoch_data(subject, "%.2i" % run_id)
+            epo.metadata["label"] = f"run_{run_id}"
             epochs.append(epo)
 
         # Quick fix for the dev_head: has to be
         # fixed before doing source reconstruction
         for epo in epochs:
-            epo.info['dev_head_t'] = epochs[0].info['dev_head_t']
+            epo.info["dev_head_t"] = epochs[0].info["dev_head_t"]
             # epo.info['nchan'] = epochs[0].info['nchan']
 
         epochs = mne.concatenate_epochs(epochs)
@@ -317,10 +320,12 @@ if __name__ == "__main__":
         evo.plot(spatial_colors=True)
 
         # Handling the data structure
-        epochs.metadata['kind'] = epochs.metadata.\
-            trial_type.apply(lambda s: eval(s)['kind'])
-        epochs.metadata['word'] = epochs.metadata.\
-            trial_type.apply(lambda s: eval(s)['word'])
+        epochs.metadata["kind"] = epochs.metadata.trial_type.apply(
+            lambda s: eval(s)["kind"]
+        )
+        epochs.metadata["word"] = epochs.metadata.trial_type.apply(
+            lambda s: eval(s)["word"]
+        )
 
         # Run a linear regression between MEG signals
         # and word frequency classification
@@ -328,7 +333,7 @@ if __name__ == "__main__":
         # X = epochs.copy().pick_types(meg='mag').get_data()  # Only mag data
         # X = epochs.copy().pick_types(meg='grad').get_data() # Only grad data
         X = epochs.get_data()  # Both mag and grad
-        y = epochs.metadata.word.apply(lambda w: zipf_frequency(w, 'fr'))
+        y = epochs.metadata.word.apply(lambda w: zipf_frequency(w, "fr"))
         R = decod(X, y)
 
         fig, ax = plt.subplots(1, figsize=[6, 6])
@@ -337,11 +342,9 @@ if __name__ == "__main__":
         report.add_evokeds(evo, titles=f"Evoked for sub {subject} ")
         report.add_figure(fig, title=f"decoding for subject {subject}")
         # report.add_figure(dec, subject, tags="word")
-        if str(PATHS.data).__contains__('BIDS'):
-            report.save("decoding_raw.html",
-                        open_browser=False, overwrite=True)
-        elif str(PATHS.data).__contains__('derivatives'):
-            report.save("decoding_filtered.html",
-                        open_browser=False, overwrite=True)
+        if str(PATHS.data).__contains__("BIDS"):
+            report.save("decoding_raw.html", open_browser=False, overwrite=True)
+        elif str(PATHS.data).__contains__("derivatives"):
+            report.save("decoding_filtered.html", open_browser=False, overwrite=True)
 
-        print('Finished!')
+        print("Finished!")
