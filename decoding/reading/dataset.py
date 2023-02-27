5da7b5d90431ad0d9a07380d4087b3df48beb51a
co <corentin.bel@protonmail.com>
2023-02-27 16:09:39 +0100

    modulated closing decoding code

--- decoding/reading/dataset.py
+++ decoding/reading/dataset.py
@@ -21,32 +21,40 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import matplotlib
 from utils import match_list
+import spacy
 
-def get_path(name='LPP_read'):
-    if name == 'LPP_read':
+
+def get_path(name="LPP_read"):
+    path_file = Path("./../../data/data_path.txt")
+    with open(path_file, "r") as f:
+        data = Path(f.readlines()[0].strip("\n"))
+    if name == "LPP_read":
         # TASK = "read"
-        data = Path("/home/is153802/workspace_LPP/data/MEG/LPP/BIDS_lecture")
+        data = data / "BIDS_lecture"
 
-    elif name == 'LPP_listen':
+    elif name == "LPP_listen":
         # TASK = "listen"
-        data = Path("/home/is153802/workspace_LPP/data/MEG/LPP/BIDS")
+        data = data / "BIDS"
     else:
-        return (f'{name} is an invalid name. \n\
-        Current options: LPP_read and LPP_listen')
+        return f"{name} is an invalid name. \n\
+        Current options: LPP_read and LPP_listen"
     return data
+
 
 # Epoching and decoding
 
 
 def epoch_data(subject, run_id, task, path, filter=True):
 
-    task = 'read' 
+    enrich = Enrich()
+
+    task = "read"
     print("Running the script on RAW data:")
     print(f"run {run_id}, subject: {subject}")
     bids_path = mne_bids.BIDSPath(
         subject=subject,
         session="01",
-        task=task, 
+        task=task,
         datatype="meg",
         root=path,
         run=run_id,
@@ -67,11 +75,11 @@ def epoch_data(subject, run_id, task, path, filter=True):
     assert Path(event_file).exists()
     # read events
     meta = pd.read_csv(event_file, sep="\t")
-    events = mne.find_events(
-        raw, stim_channel="STI101",  shortest_event=1
-    )
-    if bids_path.subject == '2':
-        word_length_meg = events[:, 2] - 2048  # Remove first event: chapter start and remove offset
+    events = mne.find_events(raw, stim_channel="STI101", shortest_event=1)
+    if bids_path.subject == "2":
+        word_length_meg = (
+            events[:, 2] - 2048
+        )  # Remove first event: chapter start and remove offset
     else:
         word_length_meg = events[:, 2]
     word_len_meta = meta.word.apply(len)
@@ -79,14 +87,32 @@ def epoch_data(subject, run_id, task, path, filter=True):
     events = events[j]
     # events = events[i]  # events = words_events[i]
     meta = meta.iloc[i].reset_index()
+    CHAPTERS = {
+        1: "1-3",
+        2: "4-6",
+        3: "7-9",
+        4: "10-12",
+        5: "13-14",
+        6: "15-19",
+        7: "20-22",
+        8: "23-25",
+        9: "26-27",
+    }
 
-    meta['start'] = events[:, 0]/raw.info['sfreq']
-    epochs = mne.Epochs(
-        raw, events, metadata=meta, tmin=-0.1, tmax=0.6, decim=20, baseline=(-0.1, 0.0)
-    )
-    epochs.load_data()
+    chapter = CHAPTERS[run_id]
+
+    meta["start"] = events[:, 0] / raw.info["sfreq"]
+    meta["condition"] = "sentence"
+    meta = meta.sort_values("start").reset_index(drop=True)
+    # add parsing data
+    path_txt = path / "../../code/data/txt_raw"
+    meta = enrich(meta, path_txt / f"ch{chapter}.txt")
+
+    epochs = mne.Epochs(raw, **mne_events(meta), decim=20, tmin=-0.2, tmax=0.8)
+    epochs = epochs['kind=="word"']
+    epochs.metadata["closing"] = epochs.metadata.closing_.fillna(0)
     epochs = epochs.pick_types(meg=True, stim=False, misc=False)
-    
+
     data = epochs.get_data()
     epochs.load_data()
 
@@ -119,6 +145,12 @@ def get_subjects(path):
     return subjects
 
 
+def mne_events(meta, raw):
+    events = np.ones((len(meta), 3), dtype=int)
+    events[:, 0] = meta.start * raw.info["sfreq"]
+    return dict(events=events, metadata=meta.reset_index())
+
+
 def concac_runs(subject, task, path):
     RUN = 9
     epochs = []
@@ -134,3 +166,107 @@ def concac_runs(subject, task, path):
     epochs = mne.concatenate_epochs(epochs)
     return epochs
 
+
+class Enrich:
+    """Enrich word dataframes (e.g. mne.Epochs.metadata)
+    with syntactic information"""
+
+    def __init__(
+        self,
+    ):
+        model = "fr_core_news_sm"
+        if not spacy.util.is_package(model):
+            spacy.cli.download(model)
+
+        self.nlp = spacy.load(model)
+
+    def __call__(self, meta, txt_file):
+
+        # read text file
+        with open(txt_file, "r") as f:
+            txt = f.read().replace("\n", "")
+
+        # parse text file
+        doc = self.nlp(txt)
+
+        # add parse information to metadata
+        parse_annots = []
+        for sent_id, sent in enumerate(doc.sents):
+            # HERE ADD ERIC DE LA CLERGERIE parser instead
+            closings = parse(sent)
+            assert len(closings) == len(sent)
+            for word, closing in zip(sent, closings):
+                parse_annots.append(
+                    dict(
+                        word_index=word.i - sent[0].i,
+                        sequence_id=sent_id,
+                        sequence_uid=str(sent),
+                        closing=closing,
+                        match_token=word.text,
+                    )
+                )
+
+        # align text file and meg metadata
+        def format_text(text):
+            for char in "jlsmtncd":
+                text = text.replace(f"{char}'", char)
+            text = text.replace("œ", "oe")
+            return text.lower()
+
+        meg_words = meta.word.fillna("######").values
+        text_words = [format_text(w.text) for w in doc]
+
+        i, j = match_list(meg_words, text_words)
+
+        # deal with missed tokens (e.g. wrong spelling, punctuation)
+        assert len(parse_annots) == len(text_words)
+        parse_annots = pd.DataFrame(parse_annots)
+        parse_annots.closing = parse_annots.closing.fillna(0)
+        parse_annots["closing_"] = 0
+        parse_annots["missed_closing"] = 0
+        missing = np.setdiff1d(range(len(parse_annots)), j)
+        for missed in missing:
+            current_closing = parse_annots.iloc[missed].closing
+            prev_word = parse_annots.iloc[[missed - 1]].index
+            if prev_word[0] >= 0:
+                parse_annots.loc[prev_word, "missed_closing"] = current_closing
+        parse_annots.closing_ = parse_annots.closing + parse_annots.missed_closing
+
+        # Add new columns to original mne.Epochs.metadata
+        # fill columns
+        columns = (
+            "word_index",
+            "sequence_id",
+            "sequence_uid",
+            "closing_",
+            "match_token",
+        )
+        for column in columns:
+            meta[column] = None
+            meta.loc[meta.iloc[i].index, column] = parse_annots[column].iloc[j].values
+        return meta
+
+
+def parse(sent):
+    "identifies the number of closing nodes"
+
+    def is_closed(node, position):
+        """JR quick code to know whether is a word is closed given a word position"""
+        if node.i > position:
+            return False
+        for child in node.children:
+            if child.i > position:
+                return False
+            if not is_closed(child, position):
+                return False
+        return True
+
+    closeds = []
+    for current in range(1, len(sent) + 1):
+        closed = 0
+        for position, word in enumerate(sent):  # [:current]
+            closed += is_closed(word, current)
+        closeds.append(closed)
+
+    closing = np.r_[np.diff(closeds), closeds[-1]]
+    return closing
